91d33e176fd13737278e88cc8bbf114418cac4f2
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2025-05-13 11:30:46 -0300

    DualShock 4 Passthrough Update

--- DS4.cs
+++ DS4.cs
@@ -41,9 +41,9 @@ namespace DSRemapper.ViGEm
         {
             ViGEmClient cli = new();
             emuController = cli.CreateDualShock4Controller(0x054C, 0x09CC);
-/*#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-            emuController.FeedbackReceived += EmuController_FeedbackReceived; //there is no other way to get it work
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos*/
+            /*#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
+                        emuController.FeedbackReceived += EmuController_FeedbackReceived; //there is no other way to get it work
+            #pragma warning restore CS0618 // El tipo o el miembro están obsoletos*/
             emuController.AutoSubmitReport = false;
         }
         /// <inheritdoc/>
@@ -139,53 +139,6 @@ namespace DSRemapper.ViGEm
                     };
                 }
                 emuController.SubmitRawReport(rawStatus.ToArray()[1..]);
-                
-                /*emuController.SetAxisValue(DualShock4Axis.LeftThumbX, (byte)(State.LX.ToSByteAxis() + 128));
-                emuController.SetAxisValue(DualShock4Axis.LeftThumbY, (byte)(State.LY.ToSByteAxis() + 128));
-                emuController.SetAxisValue(DualShock4Axis.RightThumbX, (byte)(State.RX.ToSByteAxis() + 128));
-                emuController.SetAxisValue(DualShock4Axis.RightThumbY, (byte)(State.RY.ToSByteAxis() + 128));
-
-                emuController.SetSliderValue(DualShock4Slider.LeftTrigger, State.LTrigger.ToByteTrigger());
-                emuController.SetSliderValue(DualShock4Slider.RightTrigger, State.RTrigger.ToByteTrigger());
-
-                if (State.Up && !State.Left && !State.Down && !State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.North);
-                else if (State.Up && !State.Left && !State.Down && State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.Northeast);
-                else if (!State.Up && !State.Left && !State.Down && State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.East);
-                else if (!State.Up && !State.Left && State.Down && State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.Southeast);
-                else if (!State.Up && !State.Left && State.Down && !State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.South);
-                else if (!State.Up && State.Left && State.Down && !State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.Southwest);
-                else if (!State.Up && State.Left && !State.Down && !State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.West);
-                else if (State.Up && State.Left && !State.Down && !State.Right)
-                    emuController.SetDPadDirection(DualShock4DPadDirection.Northwest);
-                else
-                    emuController.SetDPadDirection(DualShock4DPadDirection.None);
-
-                emuController.SetButtonState(DualShock4Button.Cross, State.Cross);
-                emuController.SetButtonState(DualShock4Button.Circle, State.Circle);
-                emuController.SetButtonState(DualShock4Button.Square, State.Square);
-                emuController.SetButtonState(DualShock4Button.Triangle, State.Triangle);
-
-                emuController.SetButtonState(DualShock4Button.Options, State.Options);
-                emuController.SetButtonState(DualShock4Button.Share, State.Share);
-
-                emuController.SetButtonState(DualShock4Button.ShoulderLeft, State.L1);
-                emuController.SetButtonState(DualShock4Button.ShoulderRight, State.R1);
-                emuController.SetButtonState(DualShock4Button.TriggerLeft, State.L2);
-                emuController.SetButtonState(DualShock4Button.TriggerRight, State.R2);
-                emuController.SetButtonState(DualShock4Button.ThumbLeft, State.L3);
-                emuController.SetButtonState(DualShock4Button.ThumbRight, State.R3);
-
-                emuController.SetButtonState(DualShock4SpecialButton.Ps, State.PS);
-                emuController.SetButtonState(DualShock4SpecialButton.Touchpad, State.TouchPad);
-
-                emuController.SubmitReport();*/
             }
         }
     }
